1b7ee3ed4a0152268f2df6226dfedb06dbf76be5
Vojta Tuma <vojta.tuma@gmail.com>
2024-08-07 11:07:30 +0200

    [webui] job status endpoint, redirect

--- src/forecastbox/web_ui/server.py
+++ src/forecastbox/web_ui/server.py
@@ -7,17 +7,22 @@ endpoints:
   [get]  /jobs/{job_id}	=> returns job.html with JobStatus / JobResult
 """
 
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request, status
 from typing import Annotated
-from starlette.responses import FileResponse
+from starlette.responses import FileResponse, HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+
+# from fastapi.staticfiles import StaticFiles
 import logging
 
 logger = logging.getLogger("uvicorn." + __name__)  # TODO instead configure uvicorn the same as the app
 app = FastAPI()
+# app.mount("/static", StaticFiles(directory="static"), name="static") # TODO for styles.css etc
+templates = Jinja2Templates(directory="static")
 
 
 @app.api_route("/status", methods=["GET", "HEAD"])
-def status() -> str:
+def status_check() -> str:
 	return "ok"
 
 
@@ -27,6 +32,16 @@ def index() -> FileResponse:
 
 
 @app.post("/submit")
-def submit(start_date: Annotated[str, Form()], end_date: Annotated[str, Form()]) -> str:
+def submit(request: Request, start_date: Annotated[str, Form()], end_date: Annotated[str, Form()]) -> str:
 	logger.debug(f"form params: {start_date=}, {end_date=}")
-	return "ok"
+	# TODO controller request, obtain job_id
+	job_id = "123"
+	# redirect_url = request.url_for(f"/jobs/{job_id}")
+	redirect_url = request.url_for("job_status", job_id=job_id)
+	return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+@app.get("/jobs/{job_id}", response_class=HTMLResponse)
+def job_status(request: Request, job_id: str) -> HTMLResponse:
+	# TODO controller request, fill in context
+	return templates.TemplateResponse(request=request, name="job.html", context={"job_id": job_id})
