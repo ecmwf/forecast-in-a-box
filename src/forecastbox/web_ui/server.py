9a46dbc0ed0e62bf883b2572424706159a3fc24c
Vojta Tuma <vojta.tuma@gmail.com>
2024-08-09 11:12:55 +0200

    [webui] change back to redirect

--- src/forecastbox/web_ui/server.py
+++ src/forecastbox/web_ui/server.py
@@ -8,9 +8,9 @@ endpoints:
 """
 
 from typing_extensions import Self
-from fastapi import FastAPI, Form, Request, HTTPException
+from fastapi import FastAPI, Form, Request, HTTPException, status
 from typing import Annotated, Optional
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, RedirectResponse
 import jinja2
 import pkgutil
 from forecastbox.api.controller import JobDefinition, JobStatus, JobFunctionEnum
@@ -67,8 +67,8 @@ async def index() -> str:
 	return StaticExecutionContext.get().index_html
 
 
-@app.post("/submit", response_class=HTMLResponse)
-async def submit(request: Request, start_date: Annotated[str, Form()], end_date: Annotated[str, Form()]) -> str:
+@app.post("/submit")
+async def submit(request: Request, start_date: Annotated[str, Form()], end_date: Annotated[str, Form()]) -> RedirectResponse:
 	# TODO read job function enum from some dropdown/choice in the Form
 	job_definition = JobDefinition(
 		function_name=JobFunctionEnum.hello_world, function_parameters={"start_date": start_date, "end_date": end_date}
@@ -81,11 +81,12 @@ async def submit(request: Request, start_date: Annotated[str, Form()], end_date:
 			raise HTTPException(status_code=500, detail="Internal Server Error")
 		response_json = response_raw.json()  # TODO how is this parsed? Orjson?
 		job_status = JobStatus(**response_json)
-	# TODO we dont really want to redirect since we *have* the status, but it would be nice to change the url for easier refresh
-	# redirect_url = request.url_for("job_status", job_id=job_status.job_id.job_id)
-	# return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
-	template_params = {**job_status.model_dump(), "refresh_url": f"jobs/{job_status.job_id.job_id}"}
-	return StaticExecutionContext.get().template_job.render(template_params)
+	redirect_url = request.url_for("job_status", job_id=job_status.job_id.job_id)
+	return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+	# NOTE we dont really want to redirect since we *have* the status.
+	# The code below returns that templated, but doesnt change URL so refresh wont work. Dont forget to add response_class=HTMLResponse
+	# template_params = {**job_status.model_dump(), "refresh_url": f"jobs/{job_status.job_id.job_id}"}
+	# return StaticExecutionContext.get().template_job.render(template_params)
 
 
 @app.get("/jobs/{job_id}", response_class=HTMLResponse)
