b7e10396e31f974461ee23190ff3ec13cc0b011d
Harrison Cook <harrison.cook@ecmwf.int>
2025-04-16 16:21:18 +0000

    Fix plotting

--- backend/forecastbox/products/simple.py
+++ backend/forecastbox/products/simple.py
@@ -1,6 +1,10 @@
+
+from typing import Any
+import warnings
+
 from forecastbox.products.registry import CategoryRegistry
 
-from earthkit.workflows.fluent import Payload
+from earthkit.workflows.fluent import Payload, Action
 from forecastbox.products.product import GenericTemporalProduct, GenericParamProduct
 
 from forecastbox.models import Model
@@ -17,25 +21,72 @@ try:
 except ImportError:
     EARTHKIT_PLOTS_IMPORTED = False
 
-def quickplot(field: ekd.Field, domain=None):
-    chart = ekp.Map(domain=domain)
+def quickplot(fields: ekd.FieldList, domain=None):
+    from earthkit.plots.utils import iter_utils
+    from earthkit.plots.components import layouts
+    from earthkit.plots.schemas import schema
 
-    chart.quickplot(field)
+    if not isinstance(fields, ekd.FieldList):
+        fields = ekd.FieldList.from_fields(fields)
 
-    chart.title("{variable_name} over {domain}\n"
-        "Base time: {base_time:%H:%M on %-d %B %Y}\n"
-        "Valid time: {valid_time:%H:%M on %-d %B %Y} (T+{lead_time})"
+    print(fields.ls())
+
+    groupby = 'valid_datetime'
+
+    unique_values = iter_utils.flatten(arg.metadata(groupby) for arg in fields)
+    unique_values = list(dict.fromkeys(unique_values))
+
+    grouped_data = {val: fields.sel(**{groupby: val}) for val in unique_values}
+    n_plots = len(grouped_data)
+
+    rows, columns = layouts.rows_cols(n_plots)
+    
+    figure = ekp.Figure(rows=rows, columns=columns)
+
+    for i, (group_val, group_args) in enumerate(grouped_data.items()):
+        subplot = figure.add_map(domain=domain)
+        for f in group_args:
+            subplot.quickplot(f, units = None)
+        
+        for m in schema.quickmap_subplot_workflow:
+            args = []
+            if m == "title":
+                args = ["Valid time: {valid_time:%H:%M on %-d %B %Y} (T+{lead_time})"]
+            try:
+                getattr(subplot, m)(*args)
+            except Exception as err:
+                warnings.warn(
+                    f"Failed to execute {m} on given data with: \n"
+                    f"{err}\n\n"
+                    "consider constructing the plot manually."
+                )
+
+    for m in schema.quickmap_figure_workflow:
+        try:
+            getattr(figure, m)()
+        except Exception as err:
+            warnings.warn(
+                f"Failed to execute {m} on given data with: \n"
+                f"{err}\n\n"
+                "consider constructing the plot manually."
+            )
+
+    figure.title("{variable_name} over {domain}\n"
+        # "Base time: {base_time:%H:%M on %-d %B %Y}\n"
+        # "Valid time: {valid_time:%H:%M on %-d %B %Y} (T+{lead_time})"
     )
 
-    chart.legend(label="{variable_name} ({units})")
-
-    chart.coastlines()
-    chart.gridlines()
-
-    return chart.figure
+    return figure
 
 @simple_registry("Maps")
 class MapProduct(GenericTemporalProduct):
+    """
+    Map Product.
+
+    This product is a simple wrapper around the `earthkit.plots` library to create maps.
+
+    # TODO, Add projection, and title control
+    """
     domains = ['Global', 'Europe', 'Australia', 'Malawi']
 
     description = {
@@ -48,6 +99,8 @@ class MapProduct(GenericTemporalProduct):
     }
     multiselect = {
         "param": True,
+        "step": True,
+        "domain": False,
     }
 
     @property
@@ -63,12 +116,18 @@ class MapProduct(GenericTemporalProduct):
     def mars_request(self, **kwargs):
         return super().mars_request(**kwargs)  # type:ignore
 
-    def to_graph(self, specification, source):
+    def to_graph(self, specification: dict[str, Any], source: Action):
         domain = specification.pop('domain', None)
         source = self.select_on_specification(specification, source)
 
         if ENSEMBLE_DIMENSION_NAME in source.nodes.dims:
             source = source.stack(ENSEMBLE_DIMENSION_NAME)
+        
+        if domain == 'Global':
+            domain = None
+        
+        source = source.concatenate('param')
+        source = source.concatenate('step')
 
         plots = source.map(
             Payload(
