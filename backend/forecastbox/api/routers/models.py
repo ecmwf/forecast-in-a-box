ebbc2a8b06122282590c5f0942a5a739da273c13
Harrison Cook <harrison.cook@ecmwf.int>
2025-04-22 10:28:45 +0000

    Update API - Use delete methods - Fix model download

--- backend/forecastbox/api/routers/models.py
+++ backend/forecastbox/api/routers/models.py
@@ -1,24 +1,27 @@
 """Models API Router."""
 
 from collections import defaultdict
-from fastapi import APIRouter
+from uuid import uuid4
+from fastapi import APIRouter, BackgroundTasks
 import os
 
 from functools import lru_cache
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Literal
 from pathlib import Path
+
+import httpx
+import requests
+import tempfile
+import shutil
+from pydantic import BaseModel
 
 from ..types import ModelSpecification
 from forecastbox.models import Model
 
 from forecastbox.settings import APISettings
 from forecastbox.models import Model
-
-import httpx
-import requests
-import tempfile
-import shutil
+from forecastbox.api.database import db
 
 SETTINGS = APISettings()
 
@@ -58,63 +61,178 @@ async def get_available_models() -> dict[str, list[str]]:
         models[cat].append(name)
     return models
 
+class DownloadResponse(BaseModel):
+    download_id: str | None
+    message: str
+    status: Literal['not_downloaded', 'in_progress', 'errored', 'completed']
+    progress: float
+    error: str | None = None
 
-@router.get("/downloaded/{model}")
-async def check_if_downloaded(model: str) -> bool:
+
+async def download_file(download_id: str, url: str, download_path: str) -> DownloadResponse:
+    try:
+        tempfile_path = tempfile.NamedTemporaryFile(prefix="model_", suffix=".ckpt", delete=False)
+
+        async with httpx.AsyncClient() as client_http:
+            async with client_http.stream("GET", url) as response:
+                response.raise_for_status()
+                total = int(response.headers.get("Content-Length", 0))
+                downloaded = 0
+                chunk_size = 1024
+                file_path = tempfile_path.name
+                with open(file_path, "wb") as file:
+                    async for chunk in response.aiter_bytes(chunk_size):
+                        file.write(chunk)
+                        downloaded += len(chunk)
+                        progress = int((downloaded / total) * 100) if total else 0
+                        db.update_one('model-downloads',
+                            {"_id": download_id},
+                            {"$set": {"progress": progress}},
+                        )
+
+                shutil.move(file_path, download_path)
+        db.update_one('model-downloads',
+            {"_id": download_id},
+            {"$set": {"status": "completed"}},
+        )
+    except Exception as e:
+        db.update_one('model-downloads',
+            {"_id": download_id},
+            {"$set": {"status": "errored", "error": str(e)}},
+        )
+
+@router.get("/download/{model}")
+async def check_if_downloaded(model: str) -> DownloadResponse:
     """Check if a model has been downloaded."""
 
     model_path = get_model_path(model.replace("_", "/"))
+    if model_path.exists():
+        return DownloadResponse(
+            download_id=None,
+            message="Download already completed.",
+            status='completed',
+            progress=100.00,
+        )
 
-    return model_path.exists()
+    existing_download = db.find_one('model-downloads', {"model": model})
+    if existing_download:
+        return DownloadResponse(
+            download_id=existing_download["_id"],
+            message="Download in progress.",
+            status=existing_download["status"],
+            progress=existing_download["progress"],
+            error=existing_download.get('error', None)
+        )
+        
+    return DownloadResponse(
+        download_id=None,
+        message="Model not downloaded.",
+        status='not_downloaded',
+        progress=0.00,
+    )
 
-
-@router.get("/download/{model}")
-def download(model: str) -> str:
+@router.post("/download/{model}")
+def download(model: str, background_tasks: BackgroundTasks) -> DownloadResponse:
     """Download a model."""
+
     repo = SETTINGS.model_repository
 
-    model = model.replace("_", "/")
     repo = repo.removesuffix('/')
     
-    model_path = f"{repo}/{model}.ckpt"
+    model_path = f"{repo}/{model.replace('_', '/')}.ckpt"
+
+    existing_download = db.find_one('model-downloads', {"model": model})
+    if existing_download:
+        return DownloadResponse(
+            download_id=existing_download["_id"],
+            message="Download already in progress.",
+            status=existing_download["status"],
+            progress=existing_download["progress"]
+        )
 
     model_download_path = Path(get_model_path(model.replace("_", "/")))
     model_download_path.parent.mkdir(parents=True, exist_ok=True)
 
     if model_download_path.exists():
-        return str(model_download_path)
+        return DownloadResponse(
+            download_id=None,
+            message="Download already completed.",
+            status='completed',
+            progress=100.00,
+        )
 
-    temp_download_path = tempfile.NamedTemporaryFile(suffix=".ckpt.tmp")
+    download_id = str(uuid4())
+    
+    db.insert_one('model-downloads', {
+        "_id": download_id,
+        "model": model,
+        "status": "in_progress",
+        "progress": 0
+    })
+    background_tasks.add_task(download_file, download_id, model_path, model_download_path)
+    return DownloadResponse(
+        download_id=download_id,
+        message="Download started.",
+        status='in_progress',
+        progress=0.00,
+    )
 
-    with httpx.Client() as client:
-        with client.stream("GET", model_path) as response:
-            response.raise_for_status()
-            with open(temp_download_path.name, "wb") as f:
-                for chunk in response.iter_bytes(chunk_size=8192):
-                    f.write(chunk)
+@router.delete("/{model}")
+def delete_model(model: str) -> DownloadResponse:
+    """Delete a model."""
 
-    shutil.copy(temp_download_path.name, model_download_path)
-    return str(model_download_path)
+    model_path = get_model_path(model.replace("_", "/"))
+    if not model_path.exists():
+        return DownloadResponse(
+            download_id=None,
+            message="Model not found.",
+            status='not_downloaded',
+            progress=0.00,
+        )
 
-@router.get("/install/{model}")
-def install(model: str) -> bool:
+    try:
+        os.remove(model_path)
+        if db.find_one('model-downloads', {"model": model}):
+            db.delete_one('model-downloads', {"model": model})
+    except Exception as e:
+        raise e
+    
+    return DownloadResponse(
+        download_id=None,
+        message="Model deleted.",
+        status='not_downloaded',
+        progress=0.00,
+    )
+
+class InstallResponse(BaseModel):
+    installed: bool
+    error: str | None = None
+
+@router.post("/install/{model}")
+def install(model: str) -> InstallResponse:
     from anemoi.inference.checkpoint import Checkpoint
 
     ckpt = Checkpoint(str(get_model_path(model.replace("_", "/"))))
 
     anemoi_versions = {key.replace('.','-'): val for key, val in ckpt.provenance_training()["module_versions"].items() if key.startswith("anemoi")}
     import subprocess
-    import sys
 
     BLACKLISTED_INSTALLS = ['anemoi', 'anemoi-training', 'anemoi-inference', 'anemoi-utils']
     
     try:
         packages = [f"{key}=={'.'.join(val.split('.')[:3])}" for key, val in anemoi_versions.items() if key not in BLACKLISTED_INSTALLS]
         if packages:
-            subprocess.run(["uv", "pip", "install", *packages], check=True)
+            subprocess.run(["uv", "pip", "install", *packages, '--no-cache'], check=True)
     except Exception as e:
-        raise e
-    return True
+        return InstallResponse(
+            installed=False,
+            error=str(e)
+        )
+    
+    return InstallResponse(
+        installed=True,
+        error=None
+    )
 
 
 # Model Info
@@ -152,7 +270,7 @@ async def get_model_info(model: str) -> dict[str, Any]:
     }
 
 
-@router.post("/spec/{model}")
+@router.post("/spec")
 async def get_model_spec(modelspec: ModelSpecification) -> dict[str, Any]:
     """
     Get the Qubed model spec as a json.
