bdef07f1354afd08c33aa449809b51fd33e8ccb4
Harrison Cook <harrison.cook@ecmwf.int>
2025-05-08 12:54:36 +0000

    Make Banner more obvious

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Alert, Text } from '@mantine/core';
+import { Alert, Title } from '@mantine/core';
 
 const Banner: React.FC = () => {
+    const title = <Title c="white" order={1} style={{ fontFamily: 'Nebula-Bold'}}>PROTOTYPE</Title>;
     return (
-        <Alert title="PROTOTYPE" color="red" variant="filled" >
+        <Alert title={title} color="red" variant="filled" >
             <strong>This is a prototype providing an experimental service of ECMWF products. </strong>
             DO NOT USE.
         </Alert>
