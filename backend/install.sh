272c89be9672f38b8d104b95f2b5f66262e40fee
Harrison Cook <harrison.cook@ecmwf.int>
2025-06-02 09:28:17 +0000

    Install coptrs

--- backend/install.sh
+++ backend/install.sh
@@ -5,6 +5,7 @@ uv venv --seed /app/.venv --python 3.11
 
 # Install Forecast-in-a-Box
 uv pip install --link-mode=copy /app/backend/forecastbox[all]
+uv pip install coptrs
 
 # Install ECMWF C++ Stack
 uv pip install --link-mode=copy --prerelease allow --upgrade multiolib
