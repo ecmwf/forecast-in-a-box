1ddf75edd3aa21dccb54ee0440077611093e0277
Harrison Cook <harrison.cook@ecmwf.int>
2025-07-10 10:58:55 +0000

    Update install and set default version for docker

--- backend/install.sh
+++ backend/install.sh
@@ -4,7 +4,7 @@
 if [ ! -d /app/.venv ] ; then uv venv --seed /app/.venv --python 3.11 ; fi
 
 # Install Forecast-in-a-Box
-uv pip install --link-mode=copy /app/backend/forecastbox[all]
+uv pip install --link-mode=copy --prerelease allow /app/backend/forecastbox[all] 
 uv pip install coptrs
 
 # Install ECMWF C++ Stack
