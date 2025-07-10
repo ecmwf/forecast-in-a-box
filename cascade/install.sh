37788b8702a2a7dba45147aa44fb7d5d541eef88
Harrison Cook <harrison.cook@ecmwf.int>
2025-07-10 13:26:57 +0000

    Fix install

--- cascade/install.sh
+++ cascade/install.sh
@@ -1,6 +1,6 @@
 set -euo pipefail
 
-uv venv --seed /app/.venv --python 3.11
+if [ ! -d /app/.venv ] ; then uv venv --seed /app/.venv --python 3.11 ; fi
 
 # Install Cascade
 uv pip install --link-mode=copy earthkit-workflows orjson earthkit-workflows-anemoi
