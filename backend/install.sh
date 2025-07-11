d74f3f0af0415fa3b772444341ef329ee66862b0
Harrison Cook <Harrison.cook@ecmwf.int>
2025-07-11 10:23:02 +0100

    [cicd] fix docker backend (#74)

--- backend/install.sh
+++ backend/install.sh
@@ -3,7 +3,11 @@ set -euo pipefail
 if [ ! -d /app/.venv ] ; then uv venv --seed /app/.venv --python 3.11 ; fi
 
 # Install Forecast-in-a-Box
-uv pip install --link-mode=copy --prerelease allow /app/backend/forecastbox/[all] 
+
+mkdir -p forecastbox/static && touch forecastbox/static/index.html
+echo "graft static" > MANIFEST.in
+
+uv pip install --link-mode=copy --prerelease allow ./[all] 
 uv pip install --link-mode=copy coptrs
 
 # Install ECMWF C++ Stack
