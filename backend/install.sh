e78d626bb8b042621984cc353807bcfb91520843
Harrison Cook <Harrison.cook@ecmwf.int>
2025-09-17 11:37:19 +0100

    fix: Remove C++ stack explicit install (#130)

--- backend/install.sh
+++ backend/install.sh
@@ -10,8 +10,5 @@ echo "graft static" > MANIFEST.in
 uv pip install --link-mode=copy --prerelease allow ./[all]
 uv pip install --link-mode=copy coptrs
 
-# Install ECMWF C++ Stack
-uv pip install --link-mode=copy --prerelease allow --upgrade multiolib==2.6.1.dev20250620 mir-python
-
 # Prepare the home directory for the sqlite etc
 mkdir ~/.fiab
