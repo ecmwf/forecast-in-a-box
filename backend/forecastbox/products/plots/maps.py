8d5e2749f7f041ad15dd15f57f963ec8275ca230
Harrison Cook <harrison.cook@ecmwf.int>
2025-09-18 14:25:30 +0000

    Allow arbitrary domain in map

--- backend/forecastbox/products/plots/maps.py
+++ backend/forecastbox/products/plots/maps.py
@@ -20,6 +20,7 @@ from forecastbox.products.ensemble import BaseEnsembleProduct
 from forecastbox.products.product import GenericTemporalProduct
 from forecastbox.rjsf import FieldWithUI, StringSchema, UIStringField
 
+from ..product import USER_DEFINED
 from . import plot_product_registry
 
 EARTHKIT_PLOTS_IMPORTED = True
@@ -200,11 +201,10 @@ class MapProduct(GenericTemporalProduct):
                 jsonschema=StringSchema(
                     title="Domain",
                     description="Domain of the map",
-                    enum=self.domains,
                     default="DataDefined",
                 ),
                 ui=UIStringField(
-                    widget="select",
+                    widget="text",
                 ),
             ),
         )
@@ -213,13 +213,13 @@ class MapProduct(GenericTemporalProduct):
     @property
     def model_assumptions(self):
         return {
-            "domain": self.domains,
+            "domain": '*',
             "reduce": ["True", "False"],
         }
 
     @property
     def qube(self):
-        return self.make_generic_qube(domain=self.domains, reduce=["True", "False"])
+        return self.make_generic_qube(domain=USER_DEFINED, reduce=["True", "False"])
 
     def validate_intersection(self, model: SpecifiedModel) -> bool:
         return super().validate_intersection(model) and EARTHKIT_PLOTS_IMPORTED
