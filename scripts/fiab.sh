54e0f05fe86de2cdcb2e09839507a338a765efc3
Harrison Cook <harrison.cook@ecmwf.int>
2025-10-16 13:32:27 +0000

    chore: Add earthkit-workflows-anemoi to fiab.sh

--- scripts/fiab.sh
+++ scripts/fiab.sh
@@ -96,7 +96,7 @@ maybeCreateVenv() {
     if [ "$FIAB_DEV" == 'yea' ] ; then
         uv pip install --prerelease=allow --upgrade -e .[test]
     else
-        uv pip install --prerelease=allow --upgrade pproc@git+https://github.com/ecmwf/pproc earthkit-workflows-pproc@git+https://github.com/ecmwf/earthkit-workflows-pproc 'git+https://github.com/ecmwf/anemoi-plugins-ecmwf#subdirectory=inference[opendata]' 'forecast-in-a-box[plots,webmars,test]>=0.2.2' # TODO remove prerelease once bin wheels stable, remove pproc and ekw-pproc once published
+        uv pip install --prerelease=allow --upgrade pproc@git+https://github.com/ecmwf/pproc earthkit-workflows-pproc@git+https://github.com/ecmwf/earthkit-workflows-pproc 'git+https://github.com/ecmwf/anemoi-plugins-ecmwf#subdirectory=inference[opendata]' 'forecast-in-a-box[plots,webmars,test]>=0.2.2' earthkit-workflows-anemoi # TODO remove prerelease once bin wheels stable, remove pproc and ekw-pproc once published
         export fiab__auth__passthrough=True # NOTE we dont passthrough in `dev` mode as we use it to run strict tests
     fi
 }
