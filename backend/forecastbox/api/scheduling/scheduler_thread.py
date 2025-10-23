e910756e01e3379a6aa8a25f3c54dad904eaa841
Vojta Tuma <vojtech.tuma@ecmwf.int>
2025-10-23 16:19:31 +0200

    [scheduling] finegrain scheduler status check

--- backend/forecastbox/api/scheduling/scheduler_thread.py
+++ backend/forecastbox/api/scheduling/scheduler_thread.py
@@ -17,37 +17,51 @@ many async methods shared with the backend)
 # non-async methods as well
 
 import asyncio
+import datetime as dt
 import logging
 import threading
-from datetime import datetime
 from typing import cast
 
 from forecastbox.api.execution import execute
 from forecastbox.api.scheduling.dt_utils import calculate_next_run
 from forecastbox.api.scheduling.job_utils import schedule2spec
-from forecastbox.db.schedule import (get_schedulable, insert_next_run, insert_schedule_run, mark_run_executed,
-                                     next_schedulable, delete_schedule_next_run)
+from forecastbox.config import config
+from forecastbox.db.schedule import (delete_schedule_next_run, get_schedulable, insert_next_run, insert_schedule_run,
+                                     mark_run_executed, next_schedulable)
 
 logger = logging.getLogger(__name__)
 
 # NOTE this lock can be locked externally, eg when updating schedules.
 scheduler_lock = threading.Lock()
 
+# NOTE this does not really affect how often scheduler checks for new jobs --
+# if anything is scheduled for earlier, we sleep for shorter time in advance,
+# or are `prod`ed explicitly. The actual importance of this interval is to
+# implement liveness checks correctly
+sleep_duration_min: int = 15 * 60
+
 class SchedulerThread(threading.Thread):
     def __init__(self):
         super().__init__()
-        self._stop_event = threading.Event()
+        self.stop_event = threading.Event()
         self.sleep_condition = threading.Condition()
+        self.liveness_timestamp: dt.datetime|None = None
+        self.liveness_signal = threading.Event()
+
+    def mark_alive(self) -> dt.datetime:
+        self.liveness_timestamp = dt.datetime.now()
+        self.liveness_signal.set()
+        return self.liveness_timestamp
 
     async def _try_schedule(self) -> int:
-        now = datetime.now()
+        now = self.mark_alive()
         logger.debug(f"Scheduler inquiry at {now}")
 
         schedulable_runs = await get_schedulable(now)
 
         for run in schedulable_runs:
             schedule_id_str: str = cast(str, run.schedule_id)
-            scheduled_at_dt: datetime = cast(datetime, run.scheduled_at)
+            scheduled_at_dt: dt.datetime = cast(dt.datetime, run.scheduled_at)
             schedule_next_id_str: str = cast(str, run.schedule_next_id)
             logger.debug(f"Processing scheduled run {schedule_next_id_str} for schedule {schedule_id_str} at {scheduled_at_dt}")
 
@@ -76,18 +90,17 @@ class SchedulerThread(threading.Thread):
 
         next_schedulable_at = await next_schedulable()
 
-        sleep_duration = 15 * 60
         if next_schedulable_at:
-            time_to_next_schedulable_at = (next_schedulable_at - datetime.now()).total_seconds()
+            time_to_next_schedulable_at = (next_schedulable_at - dt.datetime.now()).total_seconds()
             if time_to_next_schedulable_at > 0:
-                sleep_duration = min(time_to_next_schedulable_at, sleep_duration)
+                sleep_duration = min(time_to_next_schedulable_at, sleep_duration_min)
             else:
                 sleep_duration = 0
 
         return sleep_duration
 
     async def _run(self):
-        while not self._stop_event.is_set():
+        while not self.stop_event.is_set():
             with scheduler_lock:
                 sleep_duration = await self._try_schedule()
 
@@ -102,7 +115,7 @@ class SchedulerThread(threading.Thread):
         asyncio.run(self._run())
 
     def stop(self):
-        self._stop_event.set()
+        self.stop_event.set()
         with self.sleep_condition:
             logger.debug("Waking possibly sleeping scheduler.")
             self.sleep_condition.notify()
@@ -133,14 +146,27 @@ def prod_scheduler():
         Globals.scheduler.prod()
 
 def status_scheduler():
-    return "up" if Globals.scheduler is not None else "down"
+    if not config.api.allow_scheduler:
+        return "off"
+    if Globals.scheduler is None:
+        logger.warning("scheduler reported down due to being None")
+        return "down"
+    if not Globals.scheduler.is_alive():
+        logger.warning("scheduler reported down due to thread not being alive")
+        return "down"
+    Globals.scheduler.liveness_signal.wait(0) # we do this just for ensuring a multithread sync
+    now = dt.datetime.now()
+    if (now - Globals.scheduler.liveness_timestamp) < dt.timedelta(minutes=sleep_duration_min) * 2:
+        logger.warning("scheduler reported down due to failing liveness check")
+        return "down"
+    return "up"
 
 # NOTE this is not a class method of scheduler because its called from a different thread
 async def regenerate_schedule_next(schedule_id: str, cron_expr: str, enabled: bool) -> None:
     await delete_schedule_next_run(schedule_id)
 
     if enabled:
-        next_run_at = calculate_next_run(datetime.now(), cron_expr)
+        next_run_at = calculate_next_run(dt.datetime.now(), cron_expr)
         await insert_next_run(schedule_id, next_run_at)
         logger.debug(f"Regenerated next run for {schedule_id} at {next_run_at}")
     else:
