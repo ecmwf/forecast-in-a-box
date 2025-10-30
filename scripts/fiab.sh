b8cf9f6ab0746d4dd79163147ddb376add72ed30
Vojta Tuma <vojtech.tuma@ecmwf.int>
2025-10-30 16:06:16 +0100

    [install] add reinstall command, update docs

--- scripts/fiab.sh
+++ scripts/fiab.sh
@@ -8,30 +8,33 @@ fiab.sh
 
 The self-bootstrapping installer for Forecast in a Box
 
-This script:
+This script by default:
 1. checks for the 'uv' binary on the system, and if missing downloads into fiab
    root directory (~/.fiab)
-2. checks for a python interpreter of desired version, and if missing installs it
-3. checks for presence of uv.lock, and if missin, downloads the latest from fiab github
+2. checks for a python interpreter of desired version, and if missing installs
+3. checks for presence of pylock.toml, and if missing, downloads the latest from
+   fiab github, similarly for default config.toml
 4. checks for a venv in fiab root directory, and if missing creates it
-   and installs the fiab wheel in there from pypi
-5. executes the user's command
+   and installs the fiab package and its pylock-requirements in there from pypi
+5. runs the fiab itself, launching a web browser window by default
 
-There are currently three user's command supported:
-- regular (no extra arguments) -- launches the fiab standalone regime, with
-  frontend, controller and worker all running on the user's host
-- warmup ('fiab.sh --warmup') populates a dedicated cache with all python
-  dependencies for all tasks, so that later runs can be done offline
-- offline ('fiab.sh --offline') is as regular mode, except that no internet
-  connection is assumed. There must have been a '--warmup' run before
+There are other commands available:
+- warmup -- executes just the uv/python/pylock/venv checks, but does not
+  launch fiab itself
+- service -- as the regular run, but assumed to be executed by the systemd
+  at the system start time
+- full-reinstall -- deletes the ~/.fiab and replaces itself with the most
+  recent launcher
 
+There are many configuration options and tweaks -- inspect the code for the full
+listing.
 EOF
 }
 
 
 export FIAB_ROOT=${FIAB_ROOT:-"$HOME/.fiab"}
 # to allow forks on Macos, cf eg https://github.com/rq/rq/issues/1418
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+# export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # disabled because we switched to spawn anyway
 export EARTHKIT_DATA_CACHE_POLICY=${EARTHKIT_DATA_CACHE_POLICY:-"user"}
 export EARTHKIT_DATA_MAXIMUM_CACHE_SIZE=${EARTHKIT_DATA_MAXIMUM_CACHE_SIZE:-"50G"}
 FIAB_PY_VERSION=${FIAB_PY_VERSION:-"3.12.7"}
@@ -44,7 +47,7 @@ check() {
 		exit 1
 	fi
 	mkdir -p "$FIAB_ROOT"
-	mkdir -p "$FIAB_ROOT/uvcache" "$FIAB_ROOT/data_dir"
+	mkdir -p "$FIAB_ROOT/data_dir"
 }
 
 maybeInstallUv() {
@@ -96,22 +99,22 @@ getMostRecentRelease() {
 LOCK="${FIAB_ROOT}/pylock.toml"
 FIAB_GITHUB_FROM="${FIAB_GITHUB_FROM:-tags}" # when we want to install from a specific branch, we set this to heads & set FIAB_RELEASE to that branch
 maybeDownloadLock() {
-    most_recent=$1
+    selectedRelease=$1
     # checks whether requirements is present at fiab root, and downloads if not
     if [ ! -f $LOCK ] ; then
         >&2 echo "not found uv.lock in $LOCK, downloading"
-        >&2 echo "will download uv lock for release $most_recent"
-        lock_url=https://raw.githubusercontent.com/ecmwf/forecast-in-a-box/refs/$FIAB_GITHUB_FROM/$most_recent/install/pylock.toml
+        >&2 echo "will download uv lock for release $selectedRelease"
+        lock_url=https://raw.githubusercontent.com/ecmwf/forecast-in-a-box/refs/$FIAB_GITHUB_FROM/$selectedRelease/install/pylock.toml
 		curl -LsSf $lock_url > "$LOCK"
-        >&2 echo "$(date +%s):$(echo $most_recent | tr -d 'v')" > $LOCK.timestamp
+        >&2 echo "$(date +%s):$(echo $selectedRelease | tr -d 'v')" > $LOCK.timestamp
     fi
 }
 
 maybeGetDefaultConfig() {
-    most_recent=$1
+    selectedRelease=$1
     if [ ! -f "${FIAB_ROOT}/config.toml" ] ; then
-        >&2 echo "no config file, downloading a default for release $most_recent"
-        config_url=https://raw.githubusercontent.com/ecmwf/forecast-in-a-box/refs/$FIAB_GITHUB_FROM/$most_recent/install/config.toml
+        >&2 echo "no config file, downloading a default for release $selectedRelease"
+        config_url=https://raw.githubusercontent.com/ecmwf/forecast-in-a-box/refs/$FIAB_GITHUB_FROM/$selectedRelease/install/config.toml
 		curl -LsSf $config_url > "${FIAB_ROOT}/config.toml"
     fi
 }
@@ -122,7 +125,7 @@ updateVenv() {
     FIAB_VERSION=$(cat $LOCK.timestamp | cut -f 2 -d : )
     >&2 echo "using fiab version $FIAB_VERSION"
     uv pip install -r $LOCK
-    uv pip install --prerelease allow "forecast-in-a-box==$FIAB_VERSION"
+    uv pip install "forecast-in-a-box==$FIAB_VERSION"
     touch $VENV.timestamp
 }
 
@@ -157,42 +160,59 @@ maybePruneUvCache() {
     fi
 }
 
+ensureEnvironment() {
+    check
+    maybeInstallUv
+    maybeInstallPython
+    selectedRelease=${FIAB_RELEASE:-$(getMostRecentRelease)}
+    maybeDownloadLock $selectedRelease
+    maybeGetDefaultConfig $selectedRelease
+    maybeCreateVenv
+    maybePruneUvCache
+}
+
 # override used for eg running `pytest` instead
-ENTRYPOINT=${ENTRYPOINT:-forecastbox.standalone.entrypoint}
+COMMAND="${1:-run}"
+case "$COMMAND" in
+    "help")
+        usage
+        ;;
+    "warmup")
+        # TODO see the `--offline` for how this should be extended. In the meantime we leave this
+        # option in place just to run the base venv install and update
+        ensureEnvironment
+        ;;
+    "offline")
+        # TODO we used to have FIAB_CACHE and FIAB_OFFLINE env vars in place, with the assumption
+        # that running in warmup mode would pre-install all job dependencies. This got broken
+        # in the recent development and there is no readily accessible iteration through supported
+        # jobs. We should fix it eventually
+        >&2 echo "offline mode not supported now"
+        exit 1
+        ;;
+    "service")
+        ensureEnvironment
+        python -m forecastbox.standalone.service
+        ;;
+    "full-reinstall")
+        uv cache prune
+        rm -rf $FIAB_ROOT
+        selectedRelease=${FIAB_RELEASE:-$(getMostRecentRelease)}
+        >&2 echo "Will download launcher for release $selectedRelease"
+        launcherPath=$(readlink -f "$0")
+        launcherUrl=https://raw.githubusercontent.com/ecmwf/forecast-in-a-box/refs/$FIAB_GITHUB_FROM/$selectedRelease/scripts/fiab.sh
+		curl -LsSf $launcherUrl > "$launcherPath"
+        >&2 echo "The launcher has been updated in-place at $launcherPath. Run it again to start"
+        ;;
+    "run")
+        ensureEnvironment
+        python -m forecastbox.standalone.entrypoint
+        ;;
+    *)
+        >&2 echo "unknown command $COMMAND"
+        exit 1
+esac
 
-for arg in "$@"; do
-	case "$arg" in
-		"--help")
-			usage
-			exit 0
-			;;
-		"--warmup")
-            # TODO see the `--offline` for how this should be extended. In the meantime we leave this
-            # option in place just to run the base venv install and update
-			ENTRYPOINT=""
-			;;
-		"--offline")
-            # TODO we used to have FIAB_CACHE and FIAB_OFFLINE env vars in place, with the assumption
-            # that running in warmup mode would pre-install all job dependencies. This got broken
-            # in the recent development and there is no readily accessible iteration through supported
-            # jobs. We should fix it eventually
-            >&2 echo "offline mode not supported now"
-            exit 1
-			;;
-        "--service")
-            ENTRYPOINT="standalone.service"
-            ;;
-	esac
-done
-
-check
-maybeInstallUv
-maybeInstallPython
-selectedRelease=${FIAB_RELEASE:-$(getMostRecentRelease)}
-maybeDownloadLock $selectedRelease
-maybeGetDefaultConfig $selectedRelease
-maybeCreateVenv
-maybePruneUvCache
 
 if [ -n "$ENTRYPOINT" ] ; then
     python -m $ENTRYPOINT
