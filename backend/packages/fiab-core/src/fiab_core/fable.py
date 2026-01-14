b887b8231ce1754f134d8843974dbe1330add515
Jenny Wong <jenny.wong@ecmwf.int>
2026-01-14 13:52:00 +0000

    Change plugin toy2 to use fluent

--- backend/packages/fiab-core/src/fiab_core/fable.py
+++ backend/packages/fiab-core/src/fiab_core/fable.py
@@ -17,6 +17,8 @@ from earthkit.workflows.fluent import Action
 from pydantic import BaseModel, ConfigDict
 from typing_extensions import Self
 
+from earthkit.workflows.fluent import Action
+
 
 class BlockConfigurationOption(BaseModel):
     title: str
