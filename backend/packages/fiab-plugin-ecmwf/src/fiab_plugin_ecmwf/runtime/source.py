16ead9e3d40aab26602b389295a26b11731e9a4f
Jenny Wong <jenny.wong@ecmwf.int>
2026-02-19 10:39:08 +0000

    Allow additional kwargs to earthkit.data.from_source

--- backend/packages/fiab-plugin-ecmwf/src/fiab_plugin_ecmwf/runtime/source.py
+++ backend/packages/fiab-plugin-ecmwf/src/fiab_plugin_ecmwf/runtime/source.py
@@ -1,5 +1,5 @@
 import earthkit.data
 
 
-def earthkit_source(source: str, request: dict) -> earthkit.data.SimpleFieldList:
-    return earthkit.data.from_source(source, request=request).to_fieldlist()  # type:ignore[unresolved-attribute]
+def earthkit_source(source: str, **kwargs) -> earthkit.data.SimpleFieldList:
+    return earthkit.data.from_source(source, **kwargs).to_fieldlist()  # type:ignore[unresolved-attribute]
