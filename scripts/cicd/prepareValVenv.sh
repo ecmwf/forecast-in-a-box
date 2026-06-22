be090a4dee2bfdbb6d736b866ab1d5155c7cb7be
Vojta Tuma <vojtech.tuma@ecmwf.int>
2026-06-22 14:06:26 +0200

    [cicd] update cd.yml with the new logic

--- scripts/cicd/prepareValVenv.sh
+++ scripts/cicd/prepareValVenv.sh
@@ -3,5 +3,5 @@ uv venv valVenv
 source valVenv/bin/activate
 export UV_PROJECT_ENVIRONMENT=${SCRIPT_DIR}/buildVenv
 export UV_NO_PROJECT=1
-uv pip install pytest pytest-xdist ty # TODO how to extract these from pyproject and justfile?
-uv pip install dist/*whl
+uv pip install pytest pytest-xdist pytest-asyncio pytest-subtests pytest-mock ty # TODO how to get this from pyproject/justfile?
+uv pip install $*
