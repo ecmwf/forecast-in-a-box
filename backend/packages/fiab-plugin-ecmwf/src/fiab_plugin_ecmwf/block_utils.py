2677f712a703bde8b50f36e44fb7094efc959f3a
Harrison Cook <harrison.cook@ecmwf.int>
2026-08-25 11:42:43 +0100

    chore: fix ruff

--- backend/packages/fiab-plugin-ecmwf/src/fiab_plugin_ecmwf/block_utils.py
+++ backend/packages/fiab-plugin-ecmwf/src/fiab_plugin_ecmwf/block_utils.py
@@ -1,5 +1,5 @@
-from typing import Any
 import functools
+from typing import Any
 
 from fiab_core.fable import ConfigurationOptionId, QubedOutput
 from fiab_core.tools.blocks import BlockInstanceConfigurationError
