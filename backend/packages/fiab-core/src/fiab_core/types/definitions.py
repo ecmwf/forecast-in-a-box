a32a6abb46a44b1953e5feb118cffc6057ceb103
Jenny Wong <jenny.wong@ecmwf.int>
2026-09-23 17:25:02 +0100

    Fix formatting

--- backend/packages/fiab-core/src/fiab_core/types/definitions.py
+++ backend/packages/fiab-core/src/fiab_core/types/definitions.py
@@ -424,7 +424,7 @@ class ParameterType(StringType):
 
     def validate_convert(self, value: Any) -> str:
         try:
-            import pymetkit.paramdb # type: ignore[import]
+            import pymetkit.paramdb  # type: ignore[import]
 
             paramdb = pymetkit.paramdb.ParamDB()
             paramid = int(value)
